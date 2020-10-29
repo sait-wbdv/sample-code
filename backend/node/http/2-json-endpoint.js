5f41f25846358e0439897fd4585c3feed2ee04aa
Tony Grimes <acidtone@tonygrimes.com>
2020-10-29 05:21:30 -0600

    http 1-3

---
+++ backend/node/http/2-json-endpoint.js
@@ -0,0 +1,25 @@
+// Modified from https://gist.github.com/prof3ssorSt3v3/effc947038098045ace7a93708f8b615
+// Source: https://youtu.be/UMKS6su8HQc
+
+const http = require("http");
+
+const server = http.createServer(function(request, response) {
+
+  response.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Set MIME Type for the payload/body
+  response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});  
+  
+  // Build an object using object literal syntax
+  let dataObj = { id: 123, name: "Mandy Michael", email: "creativetext@example.com" };
+
+  // Encode js object into JSON. This needed to send data over the Internet.
+  let data = JSON.stringify(dataObj);
+
+  // Fire the trigger; this sends the response and ends the session.
+  response.end(data);
+});
+
+server.listen(8080, function() {
+  console.log("Listening on port 8080");
+});
---
+++ backend/node/http/3-path.js
@@ -0,0 +1,16 @@
+const http = require('http');
+// Load built-in `url` module
+const url = require('url');
+
+const server = http.createServer(function(request,response) {
+  // Parse the URL that was requested
+  const parsed = url.parse(request.url);
+
+  response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+
+  response.end(`<h1>Request URL: ${request.url}</h1>`);
+});
+
+server.listen(8080, function() {
+    console.log(`Listening on port 8080`);
+});
