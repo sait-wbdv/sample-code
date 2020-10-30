483736cd7e838753497f75827b2507583771f40a
Tony Grimes <acidtone@tonygrimes.com>
2020-10-29 23:26:43 -0600

    uh oh

--- backend/node/http/3-path.js
+++ backend/node/http/3-path.js
@@ -6,7 +6,7 @@ const server = http.createServer(function(request,response) {
   // Parse the URL that was requested
   const parsed = url.parse(request.url);
 
-  response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+  response.writeHead(200, {"Content-Type": "text/plain"});
 
   response.end(`<h1>Request URL: ${request.url}</h1>`);
 });
