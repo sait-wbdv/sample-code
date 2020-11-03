a44b0165babfdb82b2cf725eaad0d20f79df5800
Tony Grimes <acidtone@tonygrimes.com>
2020-11-03 05:45:58 -0700

    tweaks

--- backend/express/hello-express/server.js
+++ backend/express/hello-express/server.js
@@ -1,18 +1,23 @@
+// Load dependencies
 const path = require('path');
 const express = require('express');
 
+// Create express app
 const app = express();
 
 // app.use is for using middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Add more middleware
 app.use(function(req, res, next) {
   res.status(404);
   res.send('404: File Not Found');
 });
 
+// Set port preferrence with default
 const PORT = process.env.PORT || 3000;
 
+// Start server
 app.listen(PORT, function(){
   console.log(`Listening on port ${PORT}`);
 });
